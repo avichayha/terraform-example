6021832796bc5aadead86f26b19ae99ce9f982c8
avichayha <avichayha@cloudzone.io>
2025-11-17 21:20:45 +0200

    Update main.tf

--- demo3-infracost/main.tf
+++ demo3-infracost/main.tf
@@ -1,16 +1,49 @@
-# ------------------------------
-# S3 OBJECT WITH CHANGEABLE STORAGE CLASS (CLEAN VERSION)
-# ------------------------------
-resource "aws_s3_object" "sample_object" {
-  bucket = aws_s3_bucket.demo_bucket.id
-  key    = "example.txt"
-  source = "example.txt"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-  # <<<<<<<< COST DEMO >>>>>>>>
-  # Try changing:
-  #   STANDARD       -> STANDARD_IA
-  #   STANDARD_IA    -> ONEZONE_IA
-  #   ONEZONE_IA     -> GLACIER
-  #   GLACIER        -> GLACIER_IR
-  storage_class = "STANDARD"
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Grab Amazon Linux 2023 AMI
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# ------------------------------
+# SIMPLE EC2 COST DEMO
+# ------------------------------
+resource "aws_instance" "my_web_app" {
+  ami = data.aws_ami.al2023.id
+
+  # <<<<<<<< TRY CHANGING THIS >>>>>>>>
+  # t3.micro -> t3.large  
+  # t3.micro -> m5.large  
+  # t3.micro -> m5.2xlarge  
+  instance_type = "t3.micro"
+
+  root_block_device {
+    volume_size = 20
+
+    # <<<<<<<< TRY CHANGING THIS >>>>>>>>
+    # gp3 -> gp2  
+    # add: iops = 3000  
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "InfracostSimpleDemo"
+  }
 }
